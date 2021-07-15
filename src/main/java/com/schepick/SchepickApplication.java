7695e9b68131abf01bef6062272eb19d53db0111
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-07-15 22:08:25 +0900

    spring boot 초기세팅

---
+++ src/main/java/com/schepick/SchepickApplication.java
@@ -0,0 +1,13 @@
+package com.schepick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchepickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchepickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schepick/SchepicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schepick.schepic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchepickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
