c307ca1c551ffaf37de0bed3124eb942b9ed1177
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-07-17 00:00:47 +0900

    UserSession 생성 repo 추가

---
+++ src/main/java/com/schepick/repository/impliment/SessionUserRepository.java
@@ -0,0 +1,33 @@
+package com.schepick.repository.impliment;
+
+import com.schepick.model.User;
+import com.schepick.repository.UserRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
+@Repository
+public class SessionUserRepository implements UserRepository {
+
+    @Override
+    public Optional<User> saveDepart(String depart) {
+
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> saveGrade(byte Grade) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> saveMinorAndDoubleMajor(String minor, String doubleMajor) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> saveIsCosmos(boolean isCosmos) {
+        return Optional.empty();
+    }
+}
