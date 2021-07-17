6007bdb3560531d7c769d2d01e1aa5977df6bcb7
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-07-17 19:53:42 +0900

    lectureTime model 수정

--- src/main/java/com/schepick/model/LectureTime.java
+++ src/main/java/com/schepick/model/LectureTime.java
@@ -5,7 +5,6 @@ package com.schepick.model;
 public class LectureTime {
     private Lecture lecture;
     private String week;
-    private byte startTime;
-    private byte finishTime;
+    private byte period;
 }
 */
