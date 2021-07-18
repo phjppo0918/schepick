3a4b3b34661b76a1a545a777e4dd4eb32c40d9d3
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2021-07-18 16:41:06 +0900

    UserForm 구현중 (3)

--- src/main/resources/static/scripts/page/user/User-form.js
+++ src/main/resources/static/scripts/page/user/User-form.js
@@ -9,6 +9,36 @@ var mySwiper = new Swiper('.swiper-container', {
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
+        clickable: true,
     },
-
 });
+
+let depart = ['컴퓨터공학', '소프트웨어공학', 'IT경영', '산업경영',
+    '게임공학', '엔터테인먼트컴퓨팅', '기계공학', '기계설계공학',
+    '메카트로닉스공학', '임베디드시스템', '산업디자인공학', '미디어디자인공학',
+    '에너지·전기공학', '전자공학', '나노반도체공학', '신소재공학',
+    '생명화학공학'];
+let selectedDepart = null;
+
+function selectDepart(element, seq) {
+    if(selectedDepart != null) {
+        document.querySelector('#depart' + selectedDepart)
+             .style.backgroundColor = 'var(--color-light)';
+    }
+    document.querySelector('#inputDepart').value= depart[seq];
+    selectedDepart = seq;
+    element.style.backgroundColor = 'var(--color-middle)';
+
+}
+
+let selectedGrade = null;
+function selectGrade(element, seq) {
+    if(selectedGrade != null) {
+        document.querySelector('#grade' + selectedGrade)
+            .style.backgroundColor = 'var(--color-light)';
+    }
+    document.querySelector('#inputGrade').value = seq;
+    selectedGrade = seq;
+    element.style.backgroundColor = 'var(--color-middle)';
+
+}
